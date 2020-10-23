6e1ba7d0784b6756141130f709709b90a74ab761
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-10-23 01:49:21 -0400

    Create sign up screen

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import SignupScreen from '../screens/SignupScreen';
 import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createStackNavigator();
@@ -8,6 +9,7 @@ export default function AuthStack() {
   return (
       <Stack.Navigator initialRouteName='Login' headerMode='none'>
         <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Signup' component={SignupScreen} />
       </Stack.Navigator>
   );
 }
