f45af5cb33b238c0b80122e371d093d570f18364
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-10-30 11:56:46 -0400

    Change app theme

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { AuthProvider } from './AuthProvider';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {AuthProvider} from './AuthProvider';
 import Routes from './Routes';
 
 export default function Providers() {
   return (
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <AuthProvider>
-          <Routes />
+          <Routes/>
         </AuthProvider>
       </PaperProvider>
   );
 }
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#5b3a70',
+    accent: '#50c878',
+    background: '#f7f9fb',
+  },
+};
