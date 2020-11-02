04a3c06737460410f69f03db5aa0361c112a273b
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-11-01 19:35:24 -0500

    Implement create channel screen

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import CreateChannelScreen from '../screens/CreateChannelScreen';
+import { IconButton } from 'react-native-paper';
 
-const Stack = createStackNavigator();
+const ChatStack = createStackNavigator();
+const ModalStack = createStackNavigator();
 
 export default function HomeStack() {
   return (
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomeScreen} />
-      </Stack.Navigator>
+      <ModalStack.Navigator mode="modal" headerMode="none">
+        <ModalStack.Screen name="ChatApp" component={ChatComponent} />
+        <ModalStack.Screen name="CreateChannel" component={CreateChannelScreen} />
+      </ModalStack.Navigator>
+  );
+}
+
+function ChatComponent() {
+  return (
+      <ChatStack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#5b3a70',
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontSize: 22,
+            },
+          }}
+      >
+        <ChatStack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                  <IconButton
+                      icon="plus"
+                      size={28}
+                      color="#ffffff"
+                      onPress={() => navigation.navigate('CreateChannel')}
+                  />
+              ),
+            })}
+        />
+      </ChatStack.Navigator>
   );
 }
---
+++ src/screens/CreateChannelScreen.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { IconButton, Title } from 'react-native-paper';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import { kitty } from '../chatkitty';
+
+export default function CreateChannelScreen({ navigation }) {
+  const [channelName, setChannelName] = useState('');
+
+  function handleButtonPress() {
+    if (channelName.length > 0) {
+      kitty
+      .createChannel({
+        type: 'PUBLIC',
+        name: channelName,
+      })
+      .then(() => navigation.navigate('Home'));
+    }
+  }
+
+  return (
+      <View style={styles.rootContainer}>
+        <View style={styles.closeButtonContainer}>
+          <IconButton
+              icon="close-circle"
+              size={36}
+              color="#5b3a70"
+              onPress={() => navigation.goBack()}
+          />
+        </View>
+        <View style={styles.innerContainer}>
+          <Title style={styles.title}>Create a new channel</Title>
+          <FormInput
+              labelName="Channel Name"
+              value={channelName}
+              onChangeText={(text) => setChannelName(text)}
+              clearButtonMode="while-editing"
+          />
+          <FormButton
+              title="Create"
+              modeValue="contained"
+              labelStyle={styles.buttonLabel}
+              onPress={() => handleButtonPress()}
+              disabled={channelName.length === 0}
+          />
+        </View>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  closeButtonContainer: {
+    position: 'absolute',
+    top: 30,
+    right: 0,
+    zIndex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  buttonLabel: {
+    fontSize: 22,
+  },
+});
