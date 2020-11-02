63d1026c33b28164826adbda0f83ed1dd57ca3c4
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-11-02 00:56:16 -0500

    Loading earlier messages

--- src/screens/ChannelScreen.js
+++ src/screens/ChannelScreen.js
@@ -5,14 +5,17 @@ import Loading from '../components/Loading';
 import { AuthContext } from '../navigation/AuthProvider';
 
 export default function ChannelScreen({ route }) {
-  const { user } = useContext(AuthContext);
+  const {user} = useContext(AuthContext);
 
   const chatUser = mapUser(user);
 
-  const { channel } = route.params;
+  const {channel} = route.params;
 
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [loadEarlier, setLoadEarlier] = useState(false);
+  const [isLoadingEarlier, setIsLoadingEarlier] = useState(false);
+  const [messagePaginator, setMessagePaginator] = useState(null);
 
   function mapUser(user) {
     return {
@@ -48,6 +51,9 @@ export default function ChannelScreen({ route }) {
     .then((result) => {
       setMessages(result.paginator.items.map(mapMessage));
 
+      setMessagePaginator(result.paginator);
+      setLoadEarlier(result.paginator.hasNextPage)
+
       setLoading(false);
     });
 
@@ -55,7 +61,7 @@ export default function ChannelScreen({ route }) {
   }, []);
 
   if (loading) {
-    return <Loading />;
+    return <Loading/>;
   }
 
   async function handleSend(messages) {
@@ -65,11 +71,33 @@ export default function ChannelScreen({ route }) {
     });
   }
 
+  async function handleLoadEarlier() {
+    if (!messagePaginator.hasNextPage) {
+      setLoadEarlier(false);
+
+      return;
+    }
+
+    setIsLoadingEarlier(true);
+
+    const nextPaginator = await messagePaginator.nextPage();
+
+    setMessagePaginator(nextPaginator);
+
+    setMessages(currentMessages => GiftedChat.prepend(currentMessages,
+        nextPaginator.items.map(mapMessage)));
+
+    setIsLoadingEarlier(false);
+  }
+
   return (
       <GiftedChat
           messages={messages}
           onSend={handleSend}
           user={chatUser}
+          loadEarlier={loadEarlier}
+          isLoadingEarlier={isLoadingEarlier}
+          onLoadEarlier={handleLoadEarlier}
       />
   );
 }
