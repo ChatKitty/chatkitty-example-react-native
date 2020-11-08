bd29945e4692e4f4dc0e6de115efa40df6b6cc0a
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-11-08 12:47:38 -0500

    Handle in-app notifications

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import CreateChannelScreen from '../screens/CreateChannelScreen';
 import { IconButton } from 'react-native-paper';
 import ChannelScreen from '../screens/ChannelScreen';
 import BrowseChannelsScreen from '../screens/BrowseChannelsScreen';
+import { kitty } from '../chatkitty';
+import { withInAppNotification } from '@chatkitty/react-native-in-app-notification';
 
 const ChatStack = createStackNavigator();
 const ModalStack = createStackNavigator();
@@ -12,13 +14,35 @@ const ModalStack = createStackNavigator();
 export default function HomeStack() {
   return (
       <ModalStack.Navigator mode="modal" headerMode="none">
-        <ModalStack.Screen name="ChatApp" component={ChatComponent} />
+        <ModalStack.Screen
+            name="ChatApp"
+            component={withInAppNotification(ChatComponent)}
+        />
         <ModalStack.Screen name="CreateChannel" component={CreateChannelScreen} />
       </ModalStack.Navigator>
   );
 }
 
-function ChatComponent() {
+function ChatComponent({ navigation, showNotification }) {
+  useEffect(() => {
+    return kitty.onNotificationReceived((notification) => {
+      showNotification({
+        title: notification.title,
+        message: notification.body,
+        onPress: () => {
+          switch (notification.data.type) {
+            case 'USER:SENT:MESSAGE':
+            case 'SYSTEM:SENT:MESSAGE':
+              kitty.getChannel(notification.data.channelId).then((result) => {
+                navigation.navigate('Channel', { channel: result.channel });
+              });
+              break;
+          }
+        },
+      });
+    });
+  }, []);
+
   return (
       <ChatStack.Navigator
           screenOptions={{
@@ -63,7 +87,7 @@ function ChatComponent() {
             name="Channel"
             component={ChannelScreen}
             options={({ route }) => ({
-              title: route.params.channel.name
+              title: route.params.channel.name,
             })}
         />
       </ChatStack.Navigator>
