846442a42406b6c451a9fad1e9f62c0389444775
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-11-08 15:58:38 -0500

    Get user expo push token

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import CreateChannelScreen from '../screens/CreateChannelScreen';
@@ -12,6 +15,19 @@ const ChatStack = createStackNavigator();
 const ModalStack = createStackNavigator();
 
 export default function HomeStack() {
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      kitty.updateCurrentUser((user) => {
+        user.properties = {
+          ...user.properties,
+          'expo-push-token': token,
+        };
+
+        return user;
+      });
+    });
+  }, []);
+
   return (
       <ModalStack.Navigator mode="modal" headerMode="none">
         <ModalStack.Screen
@@ -92,4 +108,38 @@ function ChatComponent({ navigation, showNotification }) {
         />
       </ChatStack.Navigator>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+        Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
