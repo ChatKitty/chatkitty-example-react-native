a7058e4b98c5ae81cff5d03c92dec77113966b96
Aaron Nwabuoku <aerisvulpe@gmail.com>
2020-12-06 15:35:45 -0500

    WIP

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,5 +1,10 @@
+import { Video } from 'expo-av';
+import * as DocumentPicker from 'expo-document-picker';
 import React, { useContext, useEffect, useState } from 'react';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { StyleSheet } from 'react-native';
+import { Actions, Bubble, GiftedChat } from 'react-native-gifted-chat';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { View } from 'react-native-web';
 
 import { kitty } from '../chatkitty';
 import Loading from '../components/Loading';
@@ -81,6 +86,71 @@ export default function ChatScreen({ route }) {
     );
   }
 
+  const renderActions = (props) => (
+    <Actions
+      {...props}
+      containerStyle={{
+        width: 44,
+        height: 44,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 4,
+        marginRight: 4,
+        marginBottom: 0,
+      }}
+      icon={() => <Ionicons name="ios-attach" size={24} color="black" />}
+      options={{
+        'Choose Image': async () => {
+          const fileResult = await DocumentPicker.getDocumentAsync({
+            type: 'image/*',
+          });
+
+          if (fileResult.type === 'success') {
+            await kitty.sendMessage({
+              channel: channel,
+              file: fileResult.file || {
+                name: fileResult.name,
+                type: 'image/*',
+                uri: fileResult.uri,
+                size: fileResult.size,
+              },
+              progressListener: {
+                onStarted: () => {
+                  setLoading(true);
+                },
+
+                onProgress: (progress) => {
+                  console.log('Upload progress: ' + progress * 100 + '%');
+                },
+
+                onCompleted: (result) => {
+                  setLoading(false);
+                },
+              },
+            });
+          }
+        },
+        Cancel: () => {
+          console.log('Cancel');
+        },
+      }}
+      optionTintColor="#222B45"
+    />
+  );
+
+  const renderMessageVideo = (props) => {
+    const { currentMessage } = props;
+    return (
+      <View style={styles.video}>
+        <Video
+          resizeMode="contain"
+          shouldPlay={true}
+          source={{ uri: currentMessage.video }}
+        />
+      </View>
+    );
+  };
+
   if (loading) {
     return <Loading />;
   }
@@ -94,17 +164,33 @@ export default function ChatScreen({ route }) {
       isLoadingEarlier={isLoadingEarlier}
       onLoadEarlier={handleLoadEarlier}
       renderBubble={renderBubble}
+      renderActions={renderActions}
+      renderMessageVideo={renderMessageVideo}
     />
   );
 }
 
 function mapMessage(message) {
-  return {
+  let giftedMessage = {
     _id: message.id,
     text: message.body,
     createdAt: new Date(message.createdTime),
     user: mapUser(message.user),
   };
+
+  let file = message.file;
+
+  if (file) {
+    if (/image\/*/.test(file.contentType)) {
+      giftedMessage.image = file.url;
+    }
+
+    if (/video\/*/.test(file.contentType)) {
+      giftedMessage.video = file.url;
+    }
+  }
+
+  return giftedMessage;
 }
 
 function mapUser(user) {
@@ -114,3 +200,9 @@ function mapUser(user) {
     avatar: user.displayPictureUrl,
   };
 }
+
+const styles = StyleSheet.create({
+  video: {
+    padding: 20,
+  },
+});
